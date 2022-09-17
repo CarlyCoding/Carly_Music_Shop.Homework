0421057900e8901529266e3c5a9c48fb00f90280
CarlyCoding <carly_off@outlook.com>
2022-09-17 20:43:21 +0100

    Clarinet type written, write tests for Clarinet Basstype

--- src/main/java/Instruments/Clarinet.java
+++ src/main/java/Instruments/Clarinet.java
@@ -1,4 +1,17 @@
 package Instruments;
 
-public class Clarinet {
+import Interfaces.iPlay;
+
+public class Clarinet extends Instrument implements iPlay {
+
+    private String bassType;
+
+    public Clarinet(String brand, String family, String colour, String sound, double buy, double sell, String bassType) {
+        super(brand, family, colour, sound, buy, sell);
+        this.bassType = bassType;
+    }
+//Write test for the bass type as all other tests should run as per piano test.
+    public String getBassType(){
+        return bassType;
+    }
 }
