efaaba3ab11d677e9e0eafefa3421c5e0eac33ea
CarlyCoding <carly_off@outlook.com>
2022-09-17 18:42:15 +0100

    Completed Instrument super class

--- src/main/java/Instruments/Instrument.java
+++ src/main/java/Instruments/Instrument.java
@@ -1,4 +1,50 @@
 package Instruments;
 
-public class Instrument {
+import Interfaces.iPlay;
+
+public abstract class Instrument implements iPlay {
+    private String brand;
+    private String family;
+    private String colour;
+    private String sound;
+    private double buy;
+    private double sell;
+
+    public Instrument(String brand, String family, String colour, String sound, double buy, double sell){
+        this.brand = brand;
+        this.family = family;
+        this.colour = colour;
+        this.sound = sound;
+        this.buy = buy;
+        this.sell = sell;
+    }
+
+//    Notes for instruments- brand refers to Epiphone, Steinway etc., family refers to woodwind, percussion etc.
+
+    public String playInstrument(){
+        return sound;
+    }
+
+    public String getBrand(){
+        return brand;
+    }
+
+    public String getFamily(){
+        return family;
+    }
+
+    public String getColour(){
+        return colour;
+    }
+
+    public double getPriceBought(){
+        return buy;
+    }
+
+    public double getPriceToBeSold(){
+        return sell;
+    }
+
 }
+
+
