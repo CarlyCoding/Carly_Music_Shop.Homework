ec78d9f5b206f4d58ac39af3405ae384ff102736
CarlyCoding <carly_off@outlook.com>
2022-09-18 15:18:56 +0100

    All instrument testing completed

--- src/test/java/Clarinet_Test.java
+++ src/test/java/Clarinet_Test.java
@@ -1,2 +1,23 @@
+import Instruments.Clarinet;
+import Instruments.Drums;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class Clarinet_Test {
+
+    Clarinet clarinet;
+
+    @Before
+    public void setUp(){
+        clarinet = new Clarinet("Yamaha","Woodwind","Black","Fur Elise",500.00,
+        550.00,"Bass");
+    }
+
+    @Test
+    public void hasBassType(){
+        assertEquals("Bass", clarinet.getBassType());
+    }
+
 }
--- src/test/java/Drums_Test.java
+++ src/test/java/Drums_Test.java
@@ -1,2 +1,27 @@
+import Instruments.Drums;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class Drums_Test {
+
+    Drums drums;
+
+    @Before
+    public void setUp(){
+        drums = new Drums("Zildjian","Percussion", "Black", "Black Dog by Led Zepplin",
+                800.00,950.00,"Full kit", "Regular");
+    }
+
+    @Test
+    public void hasDrumType(){
+        assertEquals("Full kit", drums.getDrumType());
+    }
+
+    @Test
+    public void hasDrumSize(){
+        assertEquals("Regular", drums.getDrumSize());
+    }
+
 }
