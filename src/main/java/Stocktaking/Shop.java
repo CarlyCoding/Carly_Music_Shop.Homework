06a4a31420091ab03f2e687c35ef39c8448a283f
CarlyCoding <carly_off@outlook.com>
2022-09-18 15:45:24 +0100

    Shop class coded

--- src/main/java/Stocktaking/Shop.java
+++ src/main/java/Stocktaking/Shop.java
@@ -1,4 +1,45 @@
 package Stocktaking;
 
-public class Shop {
+
+import Instruments.Instrument;
+import Interfaces.iSell;
+
+import java.util.ArrayList;
+
+public class Shop implements iSell {
+
+    private ArrayList <Instrument> shopStock;
+    private String name;
+    private double till;
+    private double priceBought;
+    private double priceSold;
+
+    public Shop(String name, double till, double priceBought, double priceSold){
+        this.name = name;
+        this.till = till;
+        this.priceBought = priceBought;
+        this.priceSold = priceSold;
+        this.shopStock = new ArrayList<Instrument>();
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public double getTill(){
+        return till;
+    }
+
+    public double getPriceBought(){
+        return priceBought;
+    }
+
+    public double getPriceSold(){
+        return priceSold;
+    }
+
+
+    public double calculateMarkup() {
+       return (priceSold - priceBought);
+    }
 }
